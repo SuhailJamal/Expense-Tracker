c9d22c719b3d32e6ae904f89cd9c4d902e45d3f5
Suhail Jamal <119126634+SuhailJamal@users.noreply.github.com>
2024-02-01 17:48:05 +0530

    Added Close button on the Expense component

--- src/Components/Expense.jsx
+++ src/Components/Expense.jsx
@@ -1,10 +1,12 @@
 import './Expense.css'
+import { IoCloseCircleOutline } from "react-icons/io5";
 export default function Expense(props){
     return(
         <>
      <div className='container'>
         <div className='container-name'>{props.name}</div>
         <div className='container-price'>{props.price}Rs.</div>
+        <div className="closeBtn"><IoCloseCircleOutline/></div>
      </div>
         </>
     )
